5431dec8d66b6ccb40d5ae7a5d3d023754574502
bhavna jain <bhavnajain_16@hotmail.com>
2019-04-23 17:47:24 +0530

    Created Orders component

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-order-list',
@@ -7,9 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  @ViewChildren(MatTable) table: QueryList<any>;
+  public retrievedOrders: any;
+  public displayedColumns: string[] = [
+    'orderNumber', 'orderDueDate', 'customerPhone',
+    'customerBuyername', 'customerAddress', 'orderTotal', 'action'];
+  public dataSource: any;
+  public orderToBeRemoved: any;
+
+  addNewOrder = this.fb.group({
+    orderDueDate: [''],
+    customerPhone: [''],
+    customerBuyername: [''],
+    customerAddress: [''],
+    orderTotal: [''],
+  });
+
+  updateExistingOrder = this.fb.group({
+    orderDueDate: [''],
+    customerPhone: [''],
+    customerBuyername: [''],
+    customerAddress: [''],
+    orderTotal: [''],
+  });
+
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.authService.getOrders().subscribe((response: any) => {
+      this.retrievedOrders = response.orders;
+      this.dataSource = this.retrievedOrders;
+    });
   }
 
+  deleteOrder(order) {
+    this.orderToBeRemoved = order;
+  }
+
+  confirmDelete() {
+    const orderNumber = {orderNumber: this.orderToBeRemoved.orderNumber};
+    this.authService.deleteOrder(orderNumber).subscribe((response: any) => {
+      this.retrievedOrders = response;
+      this.dataSource = this.retrievedOrders;
+    });
+  }
+
+  onSubmit() {
+    this.authService.addNewOrder(this.addNewOrder.value).subscribe((response: any) => {
+    this.retrievedOrders.push(response);
+      this.dataSource = this.retrievedOrders;
+      this.table.forEach((table: MatTable<any>) => {
+        table.renderRows();
+      });
+    });
+  }
+
+  editOrder(element) {
+    this.updateExistingOrder = this.fb.group({
+      orderDueDate: [element.orderDueDate],
+      customerPhone: [element.customerPhone],
+      customerBuyername: [element.customerBuyername],
+      customerAddress: [element.customerAddress],
+      orderTotal: [element.orderTotal],
+      orderNumber: [element.orderNumber]
+    });
+  }
+
+  updateOrderDetails() {
+    console.log(this.updateExistingOrder.value);
+    this.authService.updateOrderDetails(this.updateExistingOrder.value).subscribe((response: any) => {
+      this.retrievedOrders.forEach((order: any) => {
+        if (order.orderNumber === response.orderNumber) {
+          order.orderDueDate = response.orderDueDate;
+          order.customerBuyername = response.customerBuyername;
+          order.customerAddress = response.customerAddress;
+          order.customerPhone = response.customerPhone;
+          order.orderTotal = response.orderTotal;
+        }
+      });
+      this.table.forEach((table: MatTable<any>) => {
+        table.renderRows();
+      });
+    });
+  }
 }
